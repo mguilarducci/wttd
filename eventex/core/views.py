d2050a4cabba223c24ca1e1f61ac9924a61f24f2
Matheus Guilarducci <mguilarducci@gmail.com>
2013-11-24 17:12:16 -0200

    [aula-1] Refatorando a view home
    Como um novo template foi criado, passamos a usá-lo para exibir nossa tela de boas vindas.
    Agora, na view, usamos o loader e o Context para carregar e renderizar o conteúdo do template.

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,6 +1,12 @@
 # coding: utf-8
 from django.http import HttpResponse
+from django.template import loader, Context
 
 
 def home(request):
-    return HttpResponse('Bem-vindo ao Eventex!')
+    t = loader.get_template('index.html')
+    c = Context()
+
+    content = t.render(c)
+
+    return HttpResponse(content)
