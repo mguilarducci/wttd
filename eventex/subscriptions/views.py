0627dab496ee60afb2aa1607d1904bfab7dbf570
Matheus Guilarducci <mguilarducci@gmail.com>
2013-12-01 19:12:51 -0200

    [aula-2] Refatorada as views de subscriptions

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -2,16 +2,23 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from eventex.subscriptions.forms import SubscriptionForm
-from eventex.subscriptions.models import Subscription
 
 
 def subscribe(request):
     if request.method == 'POST':
-        form = SubscriptionForm(request.POST)
-        if form.is_valid():
-            s = form.save()
-            return HttpResponseRedirect('/inscricao/%d/' % s.pk)
-        else:
-            return render(request, 'subscriptions/subscription_form.html', {'form': form})
+        return create(request)
     else:
-        return render(request, 'subscriptions/subscription_form.html', {'form': SubscriptionForm()})
+        return new(request)
+
+
+def new(request):
+    return render(request, 'subscriptions/subscription_form.html', {'form': SubscriptionForm()})
+
+
+def create(request):
+    form = SubscriptionForm(request.POST)
+    if not form.is_valid():
+        return render(request, 'subscriptions/subscription_form.html', {'form': form})
+
+    s = form.save()
+    return HttpResponseRedirect('/inscricao/%d/' % s.pk)
