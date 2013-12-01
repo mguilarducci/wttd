33f958e721ee7b219aab6c5d75a6a6c386542f92
Matheus Guilarducci <mguilarducci@gmail.com>
2013-12-01 16:21:06 -0200

    [aula-2] Criada a rota para inscrições

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -1,3 +1,13 @@
+# coding: utf-8
 from django.test import TestCase
 
-# Create your tests here.
+
+class SubscribeTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/inscricao/')
+
+    def test_get(self):
+        """
+        GET /inscricao/ must return status code 200
+        """
+        self.assertEqual(200, self.response.status_code)
